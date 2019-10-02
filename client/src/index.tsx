d7c1dd0735e5ccd9aff0fcbe848b632c08027e10
Juan <juansb827@gmail.com>
2019-10-01 19:49:16 -0500

    Set up eslint on the frontend

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/**
+ *
+ */
+import React from "react"; //
+import ReactDOM from "react-dom";
 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
-import './index.css';
-import Routes from './routes/index';
-import * as serviceWorker from './serviceWorker';
-import 'semantic-ui-css/semantic.min.css';
+import "./index.css";
+import Routes from "./routes/index";
+import * as serviceWorker from "./serviceWorker";
+import "semantic-ui-css/semantic.min.css";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/graphql',
+  uri: "http://localhost:8080/graphql"
 });
 
 const App: any = (
-    <ApolloProvider client={client}>
-        <Routes />
-    </ApolloProvider> 
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>
 );
-ReactDOM.render(App, document.getElementById('root'));
+ReactDOM.render(App, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -7,96 +7,100 @@ import { Form, Message, Header, Container } from "semantic-ui-react";
 
 const Basic = () => {
   const [login, { data, loading, error }] = useMutation(LOGIN_USER);
-  let [initialValues, setInitialValues] = useState({ email: "1", password: "1" });
+  let [initialValues, setInitialValues] = useState({
+    email: "1",
+    password: "1"
+  });
   const onSubmitHandler = async (values, actions) => {
     console.log(values);
     setInitialValues(values);
-    const response: any = await login({ variables: values});
-    const {ok, token, refreshToken} = response.data.login;
+    const response: any = await login({
+      variables: {
+        input: values
+      }
+    });
+    const { ok, token, refreshToken } = response.data.login;
     if (ok) {
-      localStorage.setItem('token', token);
-      localStorage.setItem('refreshToken', refreshToken);
+      localStorage.setItem("token", token);
+      localStorage.setItem("refreshToken", refreshToken);
     }
   };
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error :( {JSON.stringify(error)}</p>;
   return (
+    <Formik
+      initialValues={initialValues}
+      validate={values => {
+        let errors: any = {};
+        if (!values.email) {
+          errors.email = "Email is required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
 
-  <Formik
-    initialValues={initialValues}
-    validate={values => {
-      let errors: any = {};
-      if (!values.email) {
-        errors.email = "Email is required";
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      ) {
-        errors.email = "Invalid email address";
-      }
+        const password = values.password;
+        if (!password) {
+          errors.password = "Password is required";
+        } else if (password.length < 5 || password.length > 50) {
+          errors.password = "Invalid password length";
+        }
 
-      const password = values.password;
-      if (!password) {
-        errors.password = "Password is required";
-      } else if (password.length < 5 || password.length > 50) {
-        errors.password = "Invalid password length";
-      }
+        return errors;
+      }}
+      onSubmit={onSubmitHandler}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+      }) => (
+        <Container text>
+          <Header as="h2">Login {JSON.stringify(initialValues)} </Header>
+          <Form error onSubmit={handleSubmit}>
+            <Form.Input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              error={!!(errors.email && touched.email)}
+              fluid
+            />
+            <ErrorMessage name="email" component={Message} />
 
-      return errors;
-    }}
-    onSubmit={onSubmitHandler}
-  >
-    {({
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleBlur,
-      handleSubmit,
-      isSubmitting
-    }) => (
-      <Container text>
-        <Header as="h2">Login  {JSON.stringify(initialValues)} </Header>
-        <Form error onSubmit={handleSubmit}>
-          <Form.Input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-            error={!!(errors.email && touched.email)}
-            fluid
-          />
-          <ErrorMessage name="email" component={Message} />
+            <Form.Input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              error={!!(errors.password && touched.password)}
+              fluid
+            />
+            <ErrorMessage name="password" component={Message} />
 
-          
-
-          <Form.Input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-            error={!!(errors.password && touched.password)}
-            fluid
-          />
-          <ErrorMessage name="password" component={Message}/>
-         
-
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </Form>
-      </Container>
-    )}
-  </Formik>
-)};
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        </Container>
+      )}
+    </Formik>
+  );
+};
 
 export default Basic;
 
 const LOGIN_USER = gql`
-  mutation($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation($input: LoginInput!) {
+    login(input: $input) {
       ok
       token
       refreshToken
